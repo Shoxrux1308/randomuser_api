0f635ee32ae3adf3d814fc43193dbe3e4a20e3ba
Shoxrux1308 <shohruxb1308@gmail.com>
2024-11-23 17:51:07 +0500

    user

--- get_user.py
+++ get_user.py
@@ -1,3 +1,4 @@
+import requests
 def get_user():
     """
     Get user information from randomuser.me
@@ -5,3 +6,6 @@ def get_user():
     Returns:
         dict: User information
     """
+    data=requests.get("https://randomuser.me/api/").json()
+    return data["results"][0]
+print(get_user())
